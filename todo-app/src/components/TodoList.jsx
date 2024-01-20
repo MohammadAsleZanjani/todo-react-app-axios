1d07abca411aeb4ba324492f1bf8db3ca61e62e1
Mohammad Zanjani <Mohammadaslezanjani@gmail.com>
2024-01-20 18:22:10 +0330

    adding some change

--- todo-app/src/components/TodoList.jsx
+++ todo-app/src/components/TodoList.jsx
@@ -21,7 +21,7 @@ const TodoList = ({ todos, addTodo, deleteTodo }) => {
         {todos.map((todo) => (
           <li
             key={todo.id}
-            class="list-group-item d-flex justify-content-between align-items-center"
+            className="list-group-item d-flex justify-content-between align-items-center"
           >
             {todo.title}
             <input
