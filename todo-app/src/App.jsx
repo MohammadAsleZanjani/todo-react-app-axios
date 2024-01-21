95588f0d2c5f48c3dde0841f7bc13192c68984ad
Mohammad Zanjani <Mohammadaslezanjani@gmail.com>
2024-01-21 15:55:22 +0330

    complete tasks

--- todo-app/src/App.jsx
+++ todo-app/src/App.jsx
@@ -1,36 +1,10 @@
 import './App.css'
-import Loading from './components/Loading'
-import { useState, useEffect } from 'react'
-import axios from 'axios';
 import TodoList from './components/TodoList';
 
 function App() {
-  const dataUrl = 'https://jsonplaceholder.typicode.com/todos'
-  const [todos, setTodos] = useState(null)
 
-  useEffect(() => {
-    axios.get(dataUrl).then((result) => {
-      setTodos(result.data)
-    })
-  }, []) // only when components loads this one will run
+  return <TodoList /> 
 
-  const addTodo = (newTodo) => {
-    setTodos([...todos, newTodo]);
-  };
-  
-  const deleteTodo = (id) => {
-    setTodos(todos.filter((todo) => todo.id !== id));
-  };
-
-
-  return (
-    <>
-      {todos ? (
-        <TodoList todos={todos} addTodo={addTodo} deleteTodo={deleteTodo} />
-      ) : (
-        <Loading />
-      )}    </>
-  )
 }
 
 export default App
